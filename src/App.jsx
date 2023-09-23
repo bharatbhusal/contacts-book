4f94694e31b948b515b81cd5f4fe271ebf52366d
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-09-23 11:43:53 +0530

    basics done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import BookList from './components/BookList'
+import Connect from './components/Connect'
+import About from './components/About'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+import "./App.css"
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className='app'>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/books' element={<BookList />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/connect' element={<Connect />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
     )
 }
 
