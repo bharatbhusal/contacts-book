7078e75c9ff58aa46eb4d014bf07fab59c8c36b1
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-12 23:34:42 +0530

    empty phonebook

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { loader as appLoader } from "./utils/app"
 import App from "./App"
 import ErrorPage from "./error-page"
 import Contact from "./contacts"
@@ -9,12 +10,15 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />,
+        loader: appLoader,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+            },
+        ]
     },
-    {
-        path: "contacts/:contactId",
-        element: <Contact />,
-    }
 ])
 
 const root = createRoot(document.getElementById("root"))
